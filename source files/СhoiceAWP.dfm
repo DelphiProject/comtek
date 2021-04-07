fe2033994e8a25f25bb2f77395d9b0bf3326a9de
DelphiProject <yerixxlunity@gmail.com>
2021-04-07 15:20:56 +0700

    third commit

---
+++ source files/СhoiceAWP.dfm
@@ -0,0 +1,37 @@
+﻿object СhoiceAWPForm: TСhoiceAWPForm
+  Left = 227
+  Top = 108
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = #1042#1099#1073#1086#1088' '#1040#1056#1052
+  ClientHeight = 74
+  ClientWidth = 203
+  Color = clBtnFace
+  ParentFont = True
+  OldCreateOrder = True
+  Position = poScreenCenter
+  OnClose = FormClose
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object OperatorBitBtn: TBitBtn
+    Left = 8
+    Top = 8
+    Width = 185
+    Height = 25
+    Hint = #1054#1090#1082#1088#1099#1090#1100' '#1040#1056#1052' '#1054#1087#1077#1088#1072#1090#1086#1088#1072
+    Caption = #1054#1090#1082#1088#1099#1090#1100' '#1040#1056#1052' '#1054#1087#1077#1088#1072#1090#1086#1088#1072
+    TabOrder = 0
+    OnClick = OperatorBitBtnClick
+  end
+  object StatistBitBtn: TBitBtn
+    Left = 8
+    Top = 39
+    Width = 185
+    Height = 25
+    Hint = #1054#1090#1082#1088#1099#1090#1100' '#1040#1056#1052' '#1057#1090#1072#1090#1080#1089#1090#1072
+    Caption = #1054#1090#1082#1088#1099#1090#1100' '#1040#1056#1052' '#1057#1090#1072#1090#1080#1089#1090#1072
+    TabOrder = 1
+    OnClick = StatistBitBtnClick
+  end
+end
---
+++ source files/СhoiceAWP.pas
@@ -0,0 +1,74 @@
+unit ÑhoiceAWP;
+
+interface
+
+uses Winapi.Windows, System.SysUtils, System.Classes, Vcl.Graphics, Vcl.Forms,
+  Vcl.Controls, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls;
+
+type
+  TÑhoiceAWPForm = class(TForm)
+    OperatorBitBtn: TBitBtn;
+    StatistBitBtn: TBitBtn;
+    procedure FormCreate(Sender: TObject);
+    procedure OperatorBitBtnClick(Sender: TObject);
+    procedure StatistBitBtnClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+    procedure CreateMainForm;
+  public
+    { Public declarations }
+
+  end;
+
+var
+  ÑhoiceAWPForm: TÑhoiceAWPForm;
+
+implementation
+
+{$R *.dfm}
+
+uses
+  main, DataModule;
+
+{Ñîçäàíèå ãëàâíîé ôîğìû}
+procedure TÑhoiceAWPForm.CreateMainForm;
+var
+  P: Pointer;
+begin
+  P := @Application.Mainform;
+  if (not Assigned(Mainform)) then
+    Mainform := TMainForm.Create(Self);
+  Pointer(P^) := Mainform;
+  Mainform.Show;
+end;
+
+procedure TÑhoiceAWPForm.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+ CreateMainForm;
+end;
+
+procedure TÑhoiceAWPForm.FormCreate(Sender: TObject);
+begin
+  with DataModuleForm.IconsImageList do
+  begin
+    GetBitmap(0, OperatorBitBtn.Glyph);
+    GetBitmap(1, StatistBitBtn.Glyph);
+  end;
+end;
+
+procedure TÑhoiceAWPForm.OperatorBitBtnClick(Sender: TObject);
+begin
+  CreateMainForm;
+  DataModuleForm.CreateOperatorMDIChild('ÀĞÌ Îïåğàòîğà(Îêíî ¹' +
+    IntToStr(Mainform.MDIChildCount + 1)+')');
+end;
+
+procedure TÑhoiceAWPForm.StatistBitBtnClick(Sender: TObject);
+begin
+  CreateMainForm;
+  DataModuleForm.CreateStatistMDIChild('ÀĞÌ Ñòàòèñòà(Îêíî ¹' +
+    IntToStr(Mainform.MDIChildCount + 1)+')');
+end;
+
+end.
